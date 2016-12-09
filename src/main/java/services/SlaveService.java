471e1e8bc69dfd08118c09178df565fd092bf0c5
Gautam Hathi <gkh8@duke.edu>
2016-12-08 21:23:18 -0500

    added method to read entire store and documentation

--- src/main/java/services/SlaveService.java
+++ src/main/java/services/SlaveService.java
@@ -7,16 +7,31 @@ import java.util.*;
  */
 public class SlaveService {
 
+    //key/version store
     private Map<String, List<String>> store = new HashMap<String, List<String>>();
 
     public SlaveService() {
 
     }
 
+    /**
+     * Gets a list of value versions for a given key
+     *
+     * @param key The key to read versions for
+     * @return A list of versions of the value for the inputted key
+     */
     public List<String> read(String key) {
         return store.get(key);
     }
 
+    /**
+     * Replaces versions of a value with a single "resolved" value
+     *
+     * @param key The key to resolve
+     * @param seenValues The versions of the value to replace
+     * @param resolvedValue The value to replace the seen values with
+     * @return The list of values after all seen values have been resolved
+     */
     public List<String> resolve(String key, List<String> seenValues, String resolvedValue) {
         List<String> currentValues = store.get(key);
         if (currentValues == null) {
@@ -34,6 +49,13 @@ public class SlaveService {
         return store.get(key);
     }
 
+    /**
+     * Writes a version of a value for a given key
+     *
+     * @param key The key to write to
+     * @param value A value to add to the key's version list
+     * @return A list of all versions of the key's values after the new value is added
+     */
     public List<String> write (String key, String value) {
         List<String> currentVersions = store.get(key);
         if (currentVersions != null) {
@@ -47,7 +69,12 @@ public class SlaveService {
         }
     }
 
-    public void move(String path, int newSlave) {
-
+    /**
+     * Dumps entire kv store for replication
+     *
+     * @return The entire store of this replica
+     */
+    public Map<String, List<String>> getStore() {
+        return store;
     }
 }
